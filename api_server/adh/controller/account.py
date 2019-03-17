6442caa12ffaafc06b0c5797990e5de2021ac855
littlewillow <littlewillow@minet.net>
2019-03-17 22:43:21 +0100

    Python functions for account and product (get, filter, patch) Todo : create and handle errors in patch

--- api_server/adh/controller/account.py
+++ api_server/adh/controller/account.py
@@ -14,8 +14,41 @@ from adh.util.session_decorator import require_sql
 
 @require_sql
 @auth_regular_admin
-def filter_account(limit=100, offset=0, terms=None):
-    pass
+def filter_account(limit=100, offset=0, terms=None, name=None, type=None):
+    s = g.session
+    if limit < 0:
+        return "Limit must be positive", 400
+
+    q = s.query(Account)
+
+    if name and type:
+        q = q.filter(
+                (Account.name == name) &
+                (Account.type == type)
+            )
+    if name:
+        q = q.filter(Account.name == name)      
+    if type:
+        q = q.filter(Account.type == type)
+
+    if terms:
+        q = q.filter(
+            (Account.id.contains(terms)) |
+            (Account.name.contains(terms)) |
+            (Account.type.contains(terms)) |
+            (Account.actif.contains(terms))
+        )
+    count = q.count()
+    q = q.order_by(Account.name.asc())
+    q = q.offset(offset)
+    q = q.limit(limit)
+    r = q.all()
+    headers = {
+        "X-Total-Count": str(count),
+        'access-control-expose-headers': 'X-Total-Count'
+    }
+    logging.info("%s fetched the account list", g.admin.login)
+    return list(map(dict, r)), 200, headers
 
 @require_sql
 @auth_regular_admin
@@ -25,10 +58,34 @@ def create_account(body):
 @require_sql
 @auth_regular_admin
 def get_account(account_id):
-    pass
+    s = g.session
+    try:
+        logging.info("%s fetched the account %s", g.admin.login, username)
+        return dict(Account.find(s, account_id)), 200
+    except AccountNotFound:
+        return NoContent, 404
 
 @require_sql
 @auth_regular_admin
 def patch_account(account_id, body):
-    pass
+    s = g.session
 
+    update = account_exists(s, account_id)
+
+    if not update:
+        return NoContent, 404
+
+    account = Account.find(s, account_id)
+    account.start_modif_tracking()
+    try:
+        account.name = body.get("name", account.name)
+        account.type = body.get("type", account.type)
+        account.actif = body.get("actif", account.type)
+    # Erreurs non gérées
+
+    Modification.add(s, account, g.admin)
+    logging.info("%s updated the account %s\n%s",
+            g.admin.login, account_id, json.dumps(body, sort_keys=True)
+            #json.dumps prend le texte du body et le met sous le bon format
+    )
+    return NoContent, 204
--- api_server/adh/controller/product.py
+++ api_server/adh/controller/product.py
@@ -14,8 +14,34 @@ from adh.util.session_decorator import require_sql
 
 @require_sql
 @auth_regular_admin
-def filter_product(limit=100, offset=0, terms=None):
-    pass
+def filter_product(limit=100, offset=0, terms=None, name=None):
+    #pass
+    s = g.session
+    if limit < 0:
+        return "Limit must be positive", 400
+
+    q = s.query(Product)
+    if name:
+        q = q.filter(Product.name == name)
+
+    if terms:
+        q = q.filter(
+                (Product.id.contains(terms)) |
+                (Product.buying_price.contains(terms)) |
+                (Product.selling_price.contains(terms)) |
+                (Product.name.contains(terms))
+        )
+    count = q.count()
+    q = q.order_by(Product.name.asc())
+    q = q.offset(offset)
+    q = q.limit(limit)
+    r = q.all()
+    headers = {
+            "X-Total-Count": str(count),
+            'access-control-expose-headers': 'X-Total-Count'
+    }
+    logging.info("%s fetched the product list", g.admin.login)
+    return list(map(dict, r)), 200, headers
 
 @require_sql
 @auth_regular_admin
@@ -25,10 +51,10 @@ def create_product(body):
 @require_sql
 @auth_regular_admin
 def get_product(product_id):
-    pass
-
-@require_sql
-@auth_regular_admin
-def patch_product(product_id, body):
-    pass
+    s = g.session
+    try:
+        logging.info("%s fetched the product %s", g.admin.login, product_id)
+        return dict(Product.find(s, product_id)), 200
+    except ProductNotFound:
+        return NoContent, 404
 
