e87bb449aacc02324f9b8e94118f6bc37efc5c45
Nicolas Bonnet <mail@nicolasbon.net>
2019-05-18 18:04:50 +0200

    Clear Vue sample project

--- frontend_vue/src/router.js
+++ frontend_vue/src/router.js
@@ -5,19 +5,16 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/member/search',
+            name: 'member-search',
+            component: () => import(/* webpackChunkName: "about" */ './views/Member/MemberSearch.vue'),
+        },
+    ]
 })
---
+++ frontend_vue/src/views/Member/MemberSearch.vue
@@ -0,0 +1,4 @@
+<template>
+    <p>Search members.</p>
+</template>
+
