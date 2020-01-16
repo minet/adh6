00bf24ef4431bc29b12568b4cac63a5798ce9a2a
Frazew <frazew@minet.net>
2020-01-16 05:45:18 +0100

    Fix generate_api_services.sh

--- frontend_angular/generate_api_services.sh
+++ frontend_angular/generate_api_services.sh
@@ -1,5 +1,5 @@
 DIR=$(dirname "$0")
 rm -r "$DIR/src/app/api"
-wget http://central.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.15/swagger-codegen-cli-3.0.15.jar -O swagger-codegen-cli.jar
+wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.15/swagger-codegen-cli-3.0.15.jar -O swagger-codegen-cli.jar
 java -jar swagger-codegen-cli.jar generate -i "$DIR/swagger.yaml" -l typescript-angular -o "$DIR/src/app/api" --additional-properties ngVersion=7
 rm swagger-codegen-cli.jar
