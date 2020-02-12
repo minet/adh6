9e8bb1ee3cc6f7a8b2b947204d9b3ba5901e7718
Frazew <frazew@minet.net>
2020-02-12 16:04:27 +0100

    Fix frontend client_id

--- frontend_angular/src/app/config/auth.config.template.ts
+++ frontend_angular/src/app/config/auth.config.template.ts
@@ -2,7 +2,7 @@ import {AuthConfig} from 'angular-oauth2-oidc';
 
 export const authConfig: AuthConfig = {
   redirectUri: '${ADH6_URL}',
-  clientId: 'adh6_dev',
+  clientId: 'adh6',
   scope: 'openid profile offline_access roles',
   issuer: '${SSO_OAUTH2}',
   oidc: false,
