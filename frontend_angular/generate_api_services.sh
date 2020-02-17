4d19763363b6a8d046f7491b525eba5baa821408
Frazew <frazew@minet.net>
2020-02-17 23:38:30 +0100

    Change script to generate both frontend and backend code

--- frontend_angular/generate_api_services.sh
+++
@@ -1,5 +0,0 @@
-DIR=$(dirname "$0")
-rm -r "$DIR/src/app/api"
-wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.16/swagger-codegen-cli-3.0.16.jar -O swagger-codegen-cli.jar
-java -jar swagger-codegen-cli.jar generate -i "$DIR/swagger.yaml" -l typescript-angular -o "$DIR/src/app/api" --additional-properties ngVersion=7
-rm swagger-codegen-cli.jar
