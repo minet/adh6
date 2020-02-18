eb7c2cfd92502809a78e0075c0130616a0af18c1
Frazew <frazew@minet.net>
2020-02-18 03:02:40 +0100

    Update generate_api_code.sh

--- generate_api_code.sh
+++ generate_api_code.sh
@@ -1,14 +1,39 @@
 DIR=$(dirname "$0")
 [ -f swagger-codegen-cli.jar ] || wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.16/swagger-codegen-cli-3.0.16.jar -O swagger-codegen-cli.jar
 
+function generate_frontend() {
+  rm -rf "$DIR/frontend_angular/src/app/api"
+  java -jar swagger-codegen-cli.jar generate -i "$DIR/openapi/spec.yaml" -l typescript-angular -o "$DIR/frontend_angular/src/app/api" --additional-properties ngVersion=7
+}
 
-rm -rf "$DIR/frontend_angular/src/app/api"
-rm -rf "$DIR/api_server/src/entity"
+function generate_backend() {
+  java -jar swagger-codegen-cli.jar generate -i "$DIR/openapi/spec.yaml" -l python -o "$DIR/tmpsrc/"
 
-java -jar swagger-codegen-cli.jar generate -i "$DIR/openapi/spec.yaml" -l typescript-angular -o "$DIR/frontend_angular/src/app/api" --additional-properties ngVersion=7
-java -jar swagger-codegen-cli.jar generate -i "$DIR/openapi/spec.yaml" -l python -o "$DIR/tmpsrc/"
+  diff -ruN api_server/src/entity tmpsrc/swagger_client/models/ > entities.patch
+  patch -p0 < entities.patch
 
-mv "$DIR/tmpsrc/swagger_client/models/" "$DIR/api_server/src/entity"
-sed -i 's/swagger_client.models/src.entity/' "$DIR/api_server/src/entity/__init__.py"
+  sed -i 's/swagger_client.models/src.entity/' "$DIR/api_server/src/entity/__init__.py"
 
-rm -rf "$DIR/tmpsrc"
+  rm -rf "$DIR/tmpsrc"
+  rm entities.patch
+}
+
+while true; do
+  read -p "Do you wish to regenerate the frontend API code? [y/n]:" yn
+  case $yn in
+      [Yy]* ) generate_frontend; break;;
+      [Nn]* ) echo "Skipping frontend generation"; break;;
+      * ) echo "Please answer yes or no.";;
+  esac
+done
+
+while true; do
+  read -p "Do you wish to patch the backend entities? [y/n]:" yn
+  case $yn in
+      [Yy]* ) generate_backend; break;;
+      [Nn]* ) echo "Skipping backend generation"; break;;
+      * ) echo "Please answer yes or no.";;
+  esac
+done
+
+echo "Done!"
