210ce7c33d20ddc8cf91b9b82d2e997b01262c3e
othman.mhaouty <othman.mhaouty@telecom-sudparis.eu>
2020-03-15 04:03:24 +0100

    Changement d'interface !

--- frontend_angular/src/app/portail/portail.component.spec.ts
+++ frontend_angular/src/app/portail/portail.component.spec.ts
@@ -10,7 +10,7 @@ describe('PortailComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ PortailComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- frontend_angular/src/app/portail/portail.component.ts
+++ frontend_angular/src/app/portail/portail.component.ts
@@ -1,16 +1,66 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {SCOPE_LIST} from '../config/scope.config';
+import * as urlParse from 'url-parse';
+
+class AuthorizationResponse {
+  username: string;
+  scope: string;
+  client_name: string;
+  return_uri: string;
+}
 
 @Component({
   selector: 'app-portail',
   templateUrl: './portail.component.html',
-  styleUrls: ['./portail.component.css'],
   encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./portail.component.css']
 })
 export class PortailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder,) {
+  }
 
-  ngOnInit(): void {
+  public authorization$: BehaviorSubject<AuthorizationResponse> = new BehaviorSubject<AuthorizationResponse>(null);
+  public scope$: BehaviorSubject<Array<string>> = new BehaviorSubject<Array<string>>(null);
+  public consents = new Map<string, any>();
+  public return_uri = '';
+
+  SCOPE_LIST = SCOPE_LIST;
+
+  private static createHiddenElement(name: string, value: string): HTMLInputElement {
+    const hiddenField = document.createElement('input');
+    hiddenField.setAttribute('name', name);
+    hiddenField.setAttribute('value', value);
+    hiddenField.setAttribute('type', 'hidden');
+    return hiddenField;
+  }
+
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment: string) => {
+    });
+  }
+
+  onSubmit($event) {
+    const form = window.document.createElement('form');
+    form.setAttribute('method', 'POST');
+    form.setAttribute('action', this.return_uri);
+    form.setAttribute('target', '_self');
+
+    const scope = [];
+    this.consents.forEach((value: boolean, key: string) => {
+      if (value) {
+        scope.push(key);
+      }
+    });
+
+    const return_uri = urlParse(this.return_uri, true);
+    return_uri.query.scope = scope.join(' ');
+    form.appendChild(PortailComponent.createHiddenElement('confirm', '1'));
+    window.document.body.appendChild(form);
+    form.submit();
   }
 
 }
