ee08a4dfc175ab083cb100a3a8850eb00a97bbb4
Clément PARSSEGNY <cleplatypint@minet.net>
2020-03-15 03:06:15 +0100

    Test image de fond, login/mdp enleves

--- frontend_angular/src/app/portail/portail.component.ts
+++ frontend_angular/src/app/portail/portail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-portail',
   templateUrl: './portail.component.html',
-  styleUrls: ['./portail.component.css']
+  styleUrls: ['./portail.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PortailComponent implements OnInit {
 
