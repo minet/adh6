118be8281daf495dbd868a5f5008701733ac84d2
Clément PARSSEGNY <cleplatypint@minet.net>
2020-03-19 18:33:15 +0100

    Récupération des arguments de l'URL + tentative de requête API

--- frontend_angular/src/app/portail-cotisant/portail-cotisant.component.ts
+++ frontend_angular/src/app/portail-cotisant/portail-cotisant.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {SCOPE_LIST} from '../config/scope.config';
+import {map} from 'rxjs/operators';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+// @ts-ignore
 @Component({
   selector: 'app-portail-cotisant',
   templateUrl: './portail-cotisant.component.html',
@@ -8,17 +13,33 @@ import {SCOPE_LIST} from '../config/scope.config';
 })
 export class PortailCotisantComponent implements OnInit {
   constructor() { }
-  private expiration = 0;
-  private mac = 0;
-  private chambre = 0;
+  private expiration: string;
+  private mac: Observable<any>;
+  private chambre: FormGroup;
   private autre = 0;
+  private redirect: Observable<any>;
+  private route: ActivatedRoute;
+  private fb: FormBuilder;
 
+
+// tslint:disable-next-line:max-line-length
+// idée : récupérer l'adresse mac -> faire requete API pour filtrer les devices -> récupérer le "member" -> on obtient les autres infos nécessaires
   SCOPE_LIST = SCOPE_LIST;
 
+  // @ts-ignore
   ngOnInit() {
+    this.mac = this.route.params.pipe( //on récupère l'adresse mac, le redirect depuis l'URL
+      map(params => params['client_mac'])
+    );
+    this.redirect = this.route.params.pipe(
+      map(params => params['client_mac'])
+    );
+    this.chambre = this.fb.group({
+      'room': [[Validators.required, Validators.minLength(4), Validators.maxLength(4)]], }); //On récupère la chambre de l'adhérent avec une requete API (pas certain)
   }
 
-  afficheExpiration() {
+
+ /* afficheExpiration() {
     this.expiration = 1 - this.expiration;
   }
 
@@ -28,7 +49,7 @@ export class PortailCotisantComponent implements OnInit {
 
   afficheChambre() {
     this.chambre = 1 - this.chambre;
-  }
+  }   */
 
   afficheAutre() {
     this.autre = 1 - this.autre;
