90599330ac7c8a97fa7d18d55993a649ab56ce94
Clément PARSSEGNY <cleplatypint@minet.net>
2020-03-24 22:50:08 +0100

    Requete API ajoutée - Détecte si la chambre est renseignée

--- frontend_angular/src/app/portail-cotisant/portail-cotisant.component.ts
+++ frontend_angular/src/app/portail-cotisant/portail-cotisant.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {SCOPE_LIST} from '../config/scope.config';
 import {map} from 'rxjs/operators';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Observable} from 'rxjs';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractDevice, AbstractMember, Device, DeviceService, Member, MemberService} from '../api';
+
 
 // @ts-ignore
 @Component({
@@ -12,32 +12,62 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./portail-cotisant.component.css']
 })
 export class PortailCotisantComponent implements OnInit {
-  constructor() { }
-  private expiration: string;
-  private mac: Observable<any>;
-  private chambre: FormGroup;
-  private autre = 0;
-  private redirect: Observable<any>;
-  private route: ActivatedRoute;
-  private fb: FormBuilder;
 
+  private expiration: string;
+  private macDevice: string;
+  private adherent: Member;
+  private existRoom: boolean;
+  private route: ActivatedRoute;
+  private device$: Observable<Array<Device>>;
+  private mac$: Observable<string>;
+  private redirect$: Observable<any>;
+  private member$: Observable<any>;
+
+
+
+  constructor(
+    public deviceService: DeviceService,
+    public memberService: MemberService,
+  ) {}
 
 // tslint:disable-next-line:max-line-length
 // idée : récupérer l'adresse mac -> faire requete API pour filtrer les devices -> récupérer le "member" -> on obtient les autres infos nécessaires
-  SCOPE_LIST = SCOPE_LIST;
 
-  // @ts-ignore
   ngOnInit() {
-    this.mac = this.route.params.pipe( //on récupère l'adresse mac, le redirect depuis l'URL
+
+    this.mac$ = this.route.params.pipe( // on récupère l'adresse mac, le redirect depuis l'URL
       map(params => params['client_mac'])
     );
-    this.redirect = this.route.params.pipe( //on récupère le redirect
+
+    this.macDevice = this.mac$.toString();
+    this.redirect$ = this.route.params.pipe( // on récupère le redirect
       map(params => params['client_mac'])
     );
-    this.chambre = this.fb.group({
-      // tslint:disable-next-line:max-line-length
-      'room': [[Validators.required, Validators.minLength(4), Validators.maxLength(4)]], }); //On récupère la chambre de l'adhérent avec une requete API (pas certain)
-  }
+
+
+    let abstractDevice: AbstractDevice; /// création filtre de la requete API
+    if (this.mac$) {
+      abstractDevice = {
+        mac: this.macDevice
+      };
+    }
+
+    this.device$ = this.deviceService.deviceGet(1, 0, '', abstractDevice, 'body');
+
+    this.device$.subscribe(
+      devices => this.adherent = (devices[0])
+    );
+
+    let abstractMember: AbstractMember;
+    abstractMember = { id: this.adherent.id};
+    this.member$ = this.memberService.memberGet(1, 0, '', abstractMember, 'body'); // on récupére l'adhérent qui contient la chambre
+
+    this.member$.subscribe(
+      members => this.existRoom = (members.room !== undefined) // on regarde s'il y a une chambre
+    );
+    return this.existRoom; // on retourne le fait qu'il y ait une chambre
+    }
+
 
 
  /* afficheExpiration() {
@@ -51,10 +81,7 @@ export class PortailCotisantComponent implements OnInit {
   afficheChambre() {
     this.chambre = 1 - this.chambre;
   }
-
-  afficheAutre() {
-    this.autre = 1 - this.autre;
-  } */
+ */
 }
 
 
