a873987d4e32194146cd54a151f3235bb2873182
Viktor <vaktas@minet.net>
2022-02-01 18:50:06 +0100

    Remove bad import

--- api_server/test/integration/traceability/test_member.py
+++ api_server/test/integration/traceability/test_member.py
@@ -5,7 +5,7 @@ from pytest import mark
 from src.interface_adapter.http_api.auth import TESTING_CLIENT
 from test.integration.resource import logs_contains
 from test.integration.test_member import test_member_post_member_create, test_member_put_member_update, \
-    test_member_delete_existant, test_member_post_add_membership_ok, \
+    test_member_delete_existant, \
     test_member_get_logs
 
 
@@ -41,21 +41,6 @@ def test_member_log_delete(api_client, sample_member1, caplog):
         'member_manager_delete',
         user=TESTING_CLIENT,
         member_id=sample_member1.id,
-    )
-
-
-@mark.skip(reason="Not implemented")
-def test_member_log_add_membership(api_client, caplog):
-    with caplog.at_level(logging.INFO):
-        test_member_post_add_membership_ok(api_client)
-
-    assert logs_contains(
-        caplog,
-        'create_membership_record',
-        user=TESTING_CLIENT,
-        username='dubois_j',
-        duration_in_days=360,
-        start_date='2000-01-23T04:56:07+00:00',
     )
 
 
