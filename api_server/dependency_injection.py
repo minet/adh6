a8e9a1b3f5667591d379aa37610c55c0e00bc38f
Viktor <vaktas@minet.net>
2022-02-06 01:01:59 +0100

    Use __init__.py to reduce the number of import in dependeny_injection.py

--- api_server/dependency_injection.py
+++ api_server/dependency_injection.py
@@ -3,81 +3,19 @@ import abc
 
 from src.interface_adapter.http_api.default import DefaultHandler
 
+from src.use_case import managers
 from src.interface_adapter.http_api import handlers
+from src.interface_adapter.sql import repositories as sql_repositories
+from src.interface_adapter.elasticsearch import ElasticSearchRepository
+from src.interface_adapter.snmp import SwitchSNMPNetworkManager
 
-from src.interface_adapter.snmp.switch_network_manager import SwitchSNMPNetworkManager
 
-from src.interface_adapter.elasticsearch.repository import ElasticSearchRepository
-
-from src.interface_adapter.sql.account_repository import AccountSQLRepository
-from src.interface_adapter.sql.account_type_repository import AccountTypeSQLRepository
-from src.interface_adapter.sql.cashbox_repository import CashboxSQLRepository
-from src.interface_adapter.sql.device_repository import DeviceSQLRepository
 from src.interface_adapter.sql.ip_allocator import IPSQLAllocator
-from src.interface_adapter.sql.member_repository import MemberSQLRepository
-from src.interface_adapter.sql.membership_repository import MembershipSQLRepository
-from src.interface_adapter.sql.payment_method_repository import PaymentMethodSQLRepository
-from src.interface_adapter.sql.ping_repository import PingSQLRepository
-from src.interface_adapter.sql.port_repository import PortSQLRepository
-from src.interface_adapter.sql.product_repository import ProductSQLRepository
-from src.interface_adapter.sql.room_repository import RoomSQLRepository
-from src.interface_adapter.sql.switch_repository import SwitchSQLRepository
-from src.interface_adapter.sql.transaction_repository import TransactionSQLRepository
 from src.interface_adapter.sql.vlan_repository import VLANSQLRepository
 
-from src.use_case.account_manager import AccountManager
-from src.use_case.account_type_manager import AccountTypeManager
-from src.use_case.bug_report_manager import BugReportManager
-from src.use_case.cashbox_manager import CashboxManager
 from src.use_case.crud_manager import CRUDManager
-from src.use_case.device_manager import DeviceManager
-from src.use_case.health_manager import HealthManager
-from src.use_case.member_manager import MemberManager
-from src.use_case.payment_method_manager import PaymentMethodManager
-from src.use_case.port_manager import PortManager
-from src.use_case.product_manager import ProductManager
-from src.use_case.room_manager import RoomManager
-from src.use_case.stats_manager import StatsManager
-from src.use_case.switch_manager import SwitchManager
-from src.use_case.transaction_manager import TransactionManager
 from src.use_case.vlan_manager import VLANManager
 from src.use_case.interface.crud_repository import CRUDRepository
-
-_repositories = [
-    AccountSQLRepository,
-    AccountTypeSQLRepository,
-    CashboxSQLRepository,
-    DeviceSQLRepository,
-    IPSQLAllocator,
-    MemberSQLRepository,
-    MembershipSQLRepository,
-    PaymentMethodSQLRepository,
-    PingSQLRepository,
-    PortSQLRepository,
-    ProductSQLRepository,
-    RoomSQLRepository,
-    SwitchSQLRepository,
-    TransactionSQLRepository,
-    ElasticSearchRepository
-]
-
-_managers = [
-    DeviceManager,
-    HealthManager,
-    StatsManager,
-    BugReportManager,
-    ProductManager,
-    CashboxManager,
-    AccountTypeManager,
-    AccountManager,
-    TransactionManager,
-    PaymentMethodManager,
-    MemberManager,
-    RoomManager,
-    PortManager,
-    SwitchManager,
-    SwitchSNMPNetworkManager,
-]
 
 class BindingSpec(pinject.BindingSpec):
     def __init__(self, configuration, testing) -> None:
@@ -94,13 +32,14 @@ class BindingSpec(pinject.BindingSpec):
 
 
 
-_global = _repositories+_managers+handlers
+_global = sql_repositories+managers+handlers+[SwitchSNMPNetworkManager, ElasticSearchRepository]
 
 _base_interfaces = [
     abc.ABC,
     CRUDManager,
     CRUDRepository,
-    DefaultHandler
+    DefaultHandler,
+    object
 ]
 
 def get_base_class(cls):
--- api_server/src/interface_adapter/sql/__init__.py
+++ api_server/src/interface_adapter/sql/__init__.py
@@ -1,1 +1,35 @@
 # coding=utf-8
+from .account_repository import AccountSQLRepository
+from .account_type_repository import AccountTypeSQLRepository
+from .cashbox_repository import CashboxSQLRepository
+from .device_repository import DeviceSQLRepository
+from .ip_allocator import IPSQLAllocator
+from .member_repository import MemberSQLRepository
+from .membership_repository import MembershipSQLRepository
+from .money_repository import MoneySQLRepository
+from .payment_method_repository import PaymentMethodSQLRepository
+from .ping_repository import PingSQLRepository
+from .product_repository import ProductSQLRepository
+from .room_repository import RoomSQLRepository
+from .switch_repository import SwitchSQLRepository
+from .transaction_repository import TransactionSQLRepository
+from .port_repository import PortSQLRepository
+from .vlan_repository import VLANSQLRepository
+
+repositories = [
+    AccountSQLRepository,
+    AccountTypeSQLRepository,
+    CashboxSQLRepository,
+    DeviceSQLRepository,
+    IPSQLAllocator,
+    MemberSQLRepository,
+    MembershipSQLRepository,
+    PaymentMethodSQLRepository,
+    PingSQLRepository,
+    ProductSQLRepository,
+    RoomSQLRepository,
+    SwitchSQLRepository,
+    TransactionSQLRepository,
+    MoneySQLRepository,
+    PortSQLRepository
+]
