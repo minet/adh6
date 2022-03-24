14a5db2255894a7e93c2589b1a9fb177ba1da21a
Vaktas <vaktas@minet.net>
2022-03-24 21:24:28 +0100

    Add head request for the transactions as well as the devices

--- frontend_angular/src/app/api/api.module.ts
+++ frontend_angular/src/app/api/api.module.ts
@@ -16,9 +16,9 @@ import { TreasuryService } from './api/treasury.service';
 import { VlanService } from './api/vlan.service';
 
 @NgModule({
-  imports:      [],
+  imports: [],
   declarations: [],
-  exports:      [],
+  exports: [],
   providers: [
     AccountService,
     DeviceService,
@@ -30,24 +30,24 @@ import { VlanService } from './api/vlan.service';
     SwitchService,
     TransactionService,
     TreasuryService,
-    VlanService ]
+    VlanService]
 })
 export class ApiModule {
-    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
-        return {
-            ngModule: ApiModule,
-            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
-        };
-    }
+  public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
+    return {
+      ngModule: ApiModule,
+      providers: [{ provide: Configuration, useFactory: configurationFactory }]
+    };
+  }
 
-    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
-                 @Optional() http: HttpClient) {
-        if (parentModule) {
-            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
-        }
-        if (!http) {
-            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
-            'See also https://github.com/angular/angular/issues/20575');
-        }
+  constructor(@Optional() @SkipSelf() parentModule: ApiModule,
+    @Optional() http: HttpClient) {
+    if (parentModule) {
+      throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
     }
+    if (!http) {
+      throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+        'See also https://github.com/angular/angular/issues/20575');
+    }
+  }
 }
