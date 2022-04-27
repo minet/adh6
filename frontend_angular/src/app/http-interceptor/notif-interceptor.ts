f0fc8bc486626f59f3603c929eab431f72d1c5b4
Vaktas <vaktas@minet.net>
2022-04-27 02:00:59 +0200

    When error 401 redirect to the login page

--- frontend_angular/src/app/http-interceptor/notif-interceptor.ts
+++ frontend_angular/src/app/http-interceptor/notif-interceptor.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
-import {Observable, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
-import {ErrorPageService} from '../error-page.service';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorPageService } from '../error-page.service';
 import { NotificationService } from '../notification.service';
 import { Router } from '@angular/router';
 
@@ -18,33 +18,33 @@ export class NotifInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
     // Check that the request is for the API server
-      // if there is an error, notify
-      return next.handle(req).pipe(
-        catchError(response => {
-          let err = {
-            code: 200,
-            message: ''
+    // if there is an error, notify
+    return next.handle(req).pipe(
+      catchError(response => {
+        let err = {
+          code: 200,
+          message: ''
+        };
+        if (response.error.code === undefined) {
+          err = {
+            code: response.status,
+            message: response.statusText
           };
-          if (response.error.code === undefined) {
-            err = {
-              code: response.status,
-              message: response.statusText
-            };
+        } else {
+          err = response.error;
+        }
+        if (err.code === 401) {
+          this.router.navigate(['/portail'])
+        } else {
+          if (req.method === 'GET' && req.headers.get('x-critical-error') === 'true') {
+            this.errorPageService.show(err);
           } else {
-            err = response.error;
+            this.notificationService.errorNotification(+err.code, err.code + ' on ' + req.url, err.message, 3000);
           }
-          if (err.code === 401) {
-            this.router.navigate(['/dashboard'])
-          } else {
-            if (req.method === 'GET' && req.headers.get('x-critical-error') === 'true') {
-              this.errorPageService.show(err);
-            } else {
-              this.notificationService.errorNotification(+err.code, err.code + ' on ' + req.url, err.message, 3000);
-            }
-          }
-          return throwError(response);
-        }),
-      );
+        }
+        return throwError(response);
+      }),
+    );
 
   }
 }
