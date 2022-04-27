192603eca43ea5dde6045fe5e4a2fc9ac6848d3f
Vaktas <vaktas@minet.net>
2022-04-28 01:12:05 +0200

    Fix pagination member list

--- frontend_angular/src/app/app.module.ts
+++ frontend_angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
