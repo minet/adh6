fde935ece71478a6b32731496708dd722b92dea0
Vaktas <vaktas@minet.net>
2022-05-20 18:01:48 +0200

    add auth config

---
+++ frontend_angular/src/app/config/auth.config.ts
@@ -0,0 +1,21 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+const SSO_URL = "https://cas.minet.net/oidc";
+const REDIRECT_URL = "https://adh6-local.minet.net/dashboard";
+const CLIENT_ID = "adh6";
+const SCOPE = "profile";
+// const OIDC = "false";
+const RESPONSE_TYPE = "code";
+// const DEBUG = "true";
+
+export const authConfig: AuthConfig = {
+  issuer: SSO_URL,
+  redirectUri: REDIRECT_URL,
+  clientId: CLIENT_ID,
+  scope: SCOPE,
+  oidc: false,
+  responseType: RESPONSE_TYPE,
+  showDebugInformation: true,
+  dummyClientSecret: "thisisneededforApereoCAS"
+};
+
+export const authBypass = false;
