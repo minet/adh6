6e1c7e3db897770d2a9dc5c1d02b4f4db904a82f
Vaktas <vaktas@minet.net>
2022-09-18 12:17:29 +0200

    Base tracing

--- api_server/adh6/decorator/log_call.py
+++ api_server/adh6/decorator/log_call.py
@@ -4,10 +4,15 @@ Log function call decorator.
 """
 import re
 from functools import wraps
+from adh6.constants import CTX_REQUEST_URL
 
 import adh6.entity
 from adh6.misc.context import log_extra
 from adh6.misc.log import LOG
+
+from opentelemetry import trace
+
+tracer = trace.get_tracer(__name__)
 
 
 def log_call(f):
@@ -40,6 +45,8 @@ def log_call(f):
         log_kwargs["__args"] = log_args
         LOG.info(class_name + "_" + f.__name__ + "_called",
                     extra=log_extra(ctx, **log_kwargs))
-        return f(cls, ctx, *args, **kwargs)
+        
+        with tracer.start_as_current_span(type(cls).__name__ + "." + f.__name__):
+            return f(cls, ctx, *args, **kwargs)
 
     return wrapper
