7cc78147fed395dcee5e313d5cb7d647bcfba7f7
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-03-14 21:06:45 +0100

    fix: zone.js import method changed

--- frontend_angular/src/polyfills.ts
+++ frontend_angular/src/polyfills.ts
@@ -48,7 +48,7 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 
 
 /***************************************************************************************************
