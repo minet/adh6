325a1790882fc5fa5068c9051b257a3515efc0c2
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-06-29 23:02:00 +0200

    feat: use gunicorn (+ uvicorn) in production to benefit multithreading & aswgi

--- api_server/start.sh
+++ api_server/start.sh
@@ -4,4 +4,5 @@
 FLASK_APP=manage:manager flask db upgrade head
 
 # start the application
-uvicorn --port 8080 --host 0.0.0.0 main:application
+# uvicorn --port 8080 --host 0.0.0.0 main:application
+gunicorn -b 0.0.0.0:8080 -w 4 main:application -k uvicorn.workers.UvicornWorker
