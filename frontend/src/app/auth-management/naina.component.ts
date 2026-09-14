4443edf4365e50367c86bf6f673769080b436c22
Psders <theo.darvoux@telecom-sudparis.eu>
2026-09-14 18:52:19 +0200

    erroring frontend NainA

--- frontend/src/app/auth-management/naina.component.ts
+++ frontend/src/app/auth-management/naina.component.ts
@@ -1,8 +1,10 @@
 import {CommonModule} from "@angular/common";
 import {Component, OnInit} from "@angular/core";
 import {FormsModule} from "@angular/forms";
+import {HttpErrorResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Naina, NainaService} from "../api";
+import {NotificationService} from "../notification.service";
 
 @Component({
   imports: [CommonModule, FormsModule],
@@ -18,7 +20,10 @@ import {Naina, NainaService} from "../api";
       </div>
       <div class="level-right">
         <div class="level-item">
-          <button class="button is-primary" (click)="newNainA()">
+          <button
+            class="button is-primary"
+            [disabled]="login.trim() === ''"
+            (click)="newNainA()">
             Nouveau NainA
           </button>
         </div>
@@ -64,16 +69,36 @@ export class NainaComponent implements OnInit {
   public nainas$: Observable<Naina[]> = new Observable<Naina[]>();
   public login = "";
 
-  constructor(private readonly nainaService: NainaService) {}
+  constructor(
+    private readonly nainaService: NainaService,
+    private readonly notificationService: NotificationService,
+  ) {}
 
   ngOnInit(): void {
     this.refreshNainA();
   }
 
   public newNainA(): void {
-    this.nainaService
-      .nainaPost(this.login)
-      .subscribe(() => this.refreshNainA());
+    const login = this.login.trim();
+    if (login === "") {
+      return;
+    }
+    this.nainaService.nainaPost(login).subscribe({
+      next: () => {
+        this.notificationService.successNotification("NainA créé", login);
+        this.login = "";
+        this.refreshNainA();
+      },
+      error: (err: HttpErrorResponse) => {
+        if (err.status === 404) {
+          this.notificationService.errorNotification(
+            404,
+            "Utilisateur introuvable",
+            `Aucun adhérent avec le login « ${login} »`,
+          );
+        }
+      },
+    });
   }
 
   public revokeNainA(login: string): void {
